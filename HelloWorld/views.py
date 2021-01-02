9ec66b3f52fc428164cf90662a76e1f0d8922353
Karina <kserranoa16@gmail.com>
2021-01-02 17:37:25 -0600

    Completed helloWorld app with html

--- HelloWorld/views.py
+++ HelloWorld/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class homepageview(TemplateView):
+    template_name = 'home.html'
+
